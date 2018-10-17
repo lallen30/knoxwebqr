baba1b4b300039608a23d5cabafe837761b94f74
Larry Allen <lallen30@yahoo.com>
2018-10-17 17:51:02 -0400

    Changes for ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, Injectable, Injector } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
+import { Pro } from '@ionic/pro';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+Pro.init('7caab809', {
+  appVersion: '1.0.0'
+})
+
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+  ionicErrorHandler: IonicErrorHandler;
+
+  constructor(injector: Injector) {
+    try {
+      this.ionicErrorHandler = injector.get(IonicErrorHandler);
+    } catch(e) {
+      // Unable to get the IonicErrorHandler provider, ensure
+      // IonicErrorHandler has been added to the providers list below
+    }
+  }
+
+  handleError(err: any): void {
+    Pro.monitoring.handleNewError(err);
+    // Remove this if you want to disable Ionic's auto exception handling
+    // in development mode.
+    this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+  }
+}
 
 @NgModule({
   declarations: [
@@ -26,6 +53,7 @@ import { HomePage } from '../pages/home/home';
     StatusBar,
     SplashScreen,
     BarcodeScanner,
+    IonicErrorHandler,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
